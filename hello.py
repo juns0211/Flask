a6f32da5f46bcb6a4173124746cb9c833813fcb3
juns_chen <mailkill2000@gmail.com>
2022-06-14 21:47:22 +0800

    將api的post, get的method合併

--- hello.py
+++ hello.py
@@ -63,20 +63,29 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-#開post_API
-@app.route("/test", methods=['POST'])
-def test_post():
+#開API
+@app.route("/test", methods=['GET', 'POST'])
+def test():
     try:
-        insert = request.get_json()
-        user = insert['name']
-        if insert.get('id'):
-            id = insert['id']
-            obj = sql_db.mysql_db(**{'id':id, 'name':user})
+        if request.method == 'POST':
+            insert = request.get_json()
+            user = insert['name']
+            if insert.get('id'):
+                id = insert['id']
+                obj = sql_db.mysql_db(**{'id':id, 'name':user})
+            else:
+                obj = sql_db.mysql_db(**{'name':user})
+            sql_db.db.session.add(obj)
+            sql_db.db.session.commit()
+            result = sql_db.mysql_db.query.filter_by(name=user).first()
+            return Response(json.dumps({'success':True, 'message':'', 'data':{'id':result.id, 'name':result.name}}, ensure_ascii=False), status=200, mimetype='application/json')
         else:
-            obj = sql_db.mysql_db(**{'name':user})
-        sql_db.db.session.add(obj)
-        sql_db.db.session.commit()
-        return Response(json.dumps({'success':True, 'message':'', 'data':{'name':user, 'id':id} if insert.get('id') else {'name':user}}, ensure_ascii=False), status=200, mimetype='application/json')
+            id = request.args['id']
+            # result = sql_db.db.session.query(sql_db.mysql_db).filter_by(id=id).first()
+            result = sql_db.mysql_db.query.filter_by(id=id).first()
+            if not result:
+                return Response(json.dumps({'success':True, 'message':'', 'data':{}}, ensure_ascii=False), status=200, mimetype='application/json')
+            return Response(json.dumps({'success':True, 'message':'', 'data':{'id':result.id, 'name':result.name, }}, ensure_ascii=False), status=200, mimetype='application/json')
     except KeyError:
         print('\n' + traceback.format_exc())
         return Response(json.dumps({'success':False, 'message':'請檢查傳入參數是否缺少', 'data':{}}, ensure_ascii=False),status=401, mimetype='application/json')
@@ -85,22 +94,21 @@ def test_post():
         return Response(json.dumps({'success':False, 'message':'未知錯誤', 'data':{}}, ensure_ascii=False),status=401, mimetype='application/json')
 
 #開post_API
-@app.route("/test", methods=['GET'])
-def test_get():
-    try:
-        insert = request.get_json()
-        id = insert['id']
-       # result = sql_db.db.session.query(sql_db.mysql_db).filter_by(id=id).first()
-        result = sql_db.mysql_db.query.filter_by(id=id).first()
-        if not result:
-            return Response(json.dumps({'success':True, 'message':'', 'data':{}}, ensure_ascii=False), status=200, mimetype='application/json')
-        return Response(json.dumps({'success':True, 'message':'', 'data':{'name':result.name, 'id':id}}, ensure_ascii=False), status=200, mimetype='application/json')
-    except KeyError:
-        print('\n' + traceback.format_exc())
-        return Response(json.dumps({'success':False, 'message':'請檢查傳入參數是否缺少', 'data':{}}, ensure_ascii=False),status=401, mimetype='application/json')
-    except Exception:
-        print('\n' + traceback.format_exc())
-        return Response(json.dumps({'success':False, 'message':'未知錯誤', 'data':{}}, ensure_ascii=False),status=401, mimetype='application/json')
+# @app.route("/test", methods=['GET'])
+# def test_get():
+#     try:
+#         id = request.args['id']
+#        # result = sql_db.db.session.query(sql_db.mysql_db).filter_by(id=id).first()
+#         result = sql_db.mysql_db.query.filter_by(id=id).first()
+#         if not result:
+#             return Response(json.dumps({'success':True, 'message':'', 'data':{}}, ensure_ascii=False), status=200, mimetype='application/json')
+#         return Response(json.dumps({'success':True, 'message':'', 'data':{'id':id, 'name':result.name, }}, ensure_ascii=False), status=200, mimetype='application/json')
+#     except KeyError:
+#         print('\n' + traceback.format_exc())
+#         return Response(json.dumps({'success':False, 'message':'請檢查傳入參數是否缺少', 'data':{}}, ensure_ascii=False),status=401, mimetype='application/json')
+#     except Exception:
+#         print('\n' + traceback.format_exc())
+#         return Response(json.dumps({'success':False, 'message':'未知錯誤', 'data':{}}, ensure_ascii=False),status=401, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True)
