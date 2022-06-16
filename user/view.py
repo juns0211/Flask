5176fd91cd01c2a89dcfa3b25d10cf23c4adf24c
juns_chen <mailkill2000@gmail.com>
2022-06-16 16:11:22 +0800

    修正user DB名稱

--- user/view.py
+++ user/view.py
@@ -1,6 +1,6 @@
 from flask import request, Response
 from flask.views import MethodView
-from . import test_db
+from . import user_db
 import json, traceback
 
 
@@ -11,7 +11,7 @@ class test_view(MethodView):
         try:
             id = request.args['id']
             # result = sql_db.db.session.query(sql_db.mysql_db).filter_by(id=id).first()
-            result = test_db.mysql_db.query.filter_by(id=id).first()
+            result = user_db.mysql_db.query.filter_by(id=id).first()
             if not result:
                 return Response(json.dumps({'success':True, 'message':'', 'data':{}}, ensure_ascii=False), status=200, mimetype='application/json')
             return Response(json.dumps({'success':True, 'message':'', 'data':{'id':result.id, 'name':result.name}}, ensure_ascii=False), status=200, mimetype='application/json')
@@ -27,10 +27,10 @@ class test_view(MethodView):
         try:
             insert = request.get_json()
             user = insert['name']
-            obj = test_db.mysql_db(**{'name':user})
-            test_db.db.session.add(obj)
-            test_db.db.session.commit()
-            result = test_db.mysql_db.query.filter_by(name=user).first()
+            obj = user_db.mysql_db(**{'name':user})
+            user_db.db.session.add(obj)
+            user_db.db.session.commit()
+            result = user_db.mysql_db.query.filter_by(name=user).first()
             return Response(json.dumps({'success':True, 'message':'', 'data':{'id':result.id, 'name':result.name}}, ensure_ascii=False), status=200, mimetype='application/json')
         except KeyError:
             print('\n' + traceback.format_exc())
@@ -44,11 +44,11 @@ class test_view(MethodView):
             insert = request.get_json()
             user = insert['name']
             id = insert['id']
-            result = test_db.mysql_db.query.filter_by(id=id)
+            result = user_db.mysql_db.query.filter_by(id=id)
             if not result.first():
                 return Response(json.dumps({'success':True, 'message':'查無此會員', 'data':{}}, ensure_ascii=False), status=200, mimetype='application/json')
             result.update(dict(name=user))
-            test_db.db.session.commit()
+            user_db.db.session.commit()
             return Response(json.dumps({'success':True, 'message':'', 'data':{'id':id, 'name':user}}, ensure_ascii=False), status=200, mimetype='application/json')
         except KeyError:
             print('\n' + traceback.format_exc())
@@ -64,12 +64,12 @@ class test_view(MethodView):
             insert = request.get_json()
             ids = insert['id']
             for id in ids:
-                resp = test_db.mysql_db.query.filter_by(id=id)
+                resp = user_db.mysql_db.query.filter_by(id=id)
                 if not resp.first():
                     continue
                 result.append({'id':resp.first().id, 'name':resp.first().name})
                 resp.delete()
-            test_db.db.session.commit()
+            user_db.db.session.commit()
             return Response(json.dumps({'success':True, 'message':'刪除成功', 'data':result}, ensure_ascii=False), status=200, mimetype='application/json')
         except KeyError:
             print('\n' + traceback.format_exc())
